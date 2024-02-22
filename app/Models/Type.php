aa8432d6cb5c264c4504126b24130b82b3b9c73e
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-22 17:41:24 +0100

    update: model

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    public function project()
+    {
+        return $this->hasOne(Project::class);
+    }
+
 }
+
