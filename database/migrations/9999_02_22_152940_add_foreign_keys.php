1551f491dcf1d445e43f0b74df768c2d2a41d157
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-22 16:42:44 +0100

    create: function up

--- database/migrations/9999_02_22_152940_add_foreign_keys.php
+++ database/migrations/9999_02_22_152940_add_foreign_keys.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,7 +12,11 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreignId('type_id')->constrained();
+        });
+
     }
 
     /**
@@ -23,6 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+
+
+
     }
 };
