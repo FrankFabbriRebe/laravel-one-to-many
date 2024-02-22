f937a79c16324dcecfcbf056e669f39a0542bfad
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-22 15:10:53 +0100

    import: model

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// import model
+use App\Models\Project;
+
 class ProjectsController extends Controller
 {
     return view('pages.index');
