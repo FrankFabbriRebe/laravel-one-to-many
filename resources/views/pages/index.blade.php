9b1f61d57bf8307d5e97b29b8cba0839ad5cf424
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-23 14:35:44 +0100

    update: front-end

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,7 +10,9 @@
     @foreach ($projects as $project)
     <li>
         <h3>Project name: </h3>
-        <span>{{ $project -> name }}: </span>
+        <span>{{ $project -> name }}</span>
+        <br>
+        <strong>Type name: </strong>
         <span>{{ $project -> type -> name}}</span>
     </li>
     @endforeach
